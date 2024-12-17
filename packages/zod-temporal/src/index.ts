9f998f9336cf9989fa696a44aac02a5e4574a88d
Ian Macalinao <github@igm.pub>
2024-12-17 15:48:30 +0800

    export the ZodTemporal type

--- packages/zod-temporal/src/index.ts
+++ packages/zod-temporal/src/index.ts
@@ -3,4 +3,5 @@ export * from "./instant.js";
 export * from "./plainDate.js";
 export * from "./plainDateTime.js";
 export * from "./plainTime.js";
+export type { ZodTemporal } from "./temporalValidator.js";
 export * from "./zonedDateTime.js";
